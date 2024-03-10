8e526598d733331143cb1e25004864c6a29410de
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-10 23:06:00 +0530

    Update App.java

--- SnakeLadder/Codes/App.java
+++ SnakeLadder/Codes/App.java
@@ -1,28 +1,34 @@
+import board.Board;
 import coordinates.Coordinates;
+import dice.Dice;
+import game.Game;
+import java.util.*;
 import jumper.Jumper;
+import player.Player;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        HashMap<String, Jumper> map = new HashMap<>();
+        map.put("6", new Jumper(new Coordinates(9,5), new Coordinates(6,6)));
+        map.put("97", new Jumper(new Coordinates(0,1), new Coordinates(9,9)));
+        map.put("21", new Jumper(new Coordinates(7,0), new Coordinates(0,9)));
+        map.put("64", new Jumper(new Coordinates(3,3), new Coordinates(7,4)));
         
-        // Player p1 = new Player();
-        // p1.setPlayerDetailsFromUserInput(p1);
-        // Player p2 = new Player();
-        // p2.setPlayerDetailsFromUserInput(p2);
+        Player p1 = new Player();
+        p1.setPlayerDetailsFromUserInput(p1);
+        Player p2 = new Player();
+        p2.setPlayerDetailsFromUserInput(p2);
 
-        // System.out.println("Player name: " + p1.getPlayerName());
-        // System.out.println("Player name: " + p2.getPlayerName());
+        System.out.println("Player name: " + p1.getPlayerName());
+        System.out.println("Player name: " + p2.getPlayerName());
 
-        // Dice dice = new Dice();
+        Board b = new Board(10, map);
+        b.printBoard();
 
-        // System.out.println(dice.diceRoll());
-
-        Coordinates c = new Coordinates(8, 7);
-        Coordinates d = new Coordinates(6, 7);
-        // System.out.println(c.printCoordinates());
-
-        Jumper j = new Jumper(c, d);
-        System.out.println(j.getJumperName());
-
+        Game game = new Game(b, new Player[] {p1,p2}, new Dice());
+        game.play();
     }
     
 }
