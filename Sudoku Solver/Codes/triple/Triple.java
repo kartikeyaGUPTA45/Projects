384de9096b95af9f3ce5f99a3642231fe1935457
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 11:07:00 +0530

    Create Triple.java

---
+++ Sudoku Solver/Codes/triple/Triple.java
@@ -0,0 +1,13 @@
+package triple;
+
+public class Triple {
+    public int row;
+    public int col; 
+    public int val;
+
+    public Triple(int row, int col, int val) {
+        this.row = row;
+        this.col = col;
+        this.val = val;
+    }
+}
