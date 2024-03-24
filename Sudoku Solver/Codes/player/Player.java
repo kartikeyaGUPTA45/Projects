58bc1b649f070a5ca44d828a8926aa5f0fad565d
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 11:06:16 +0530

    Create Player.java

---
+++ Sudoku Solver/Codes/player/Player.java
@@ -0,0 +1,17 @@
+package player;
+
+public class Player {
+    String name;
+    int age;
+
+
+    public Player (String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getPlayerName() {
+        return this.name;
+    }
+}
