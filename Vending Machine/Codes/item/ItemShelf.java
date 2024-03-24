953bb224dc3dd962e5192f59a9cb29df8f9ee34f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 09:30:31 +0530

    Create ItemShelf.java

---
+++ Vending Machine/Codes/item/ItemShelf.java
@@ -0,0 +1,34 @@
+package item;
+
+public class ItemShelf {
+// Item-> ItemType, price, code, soldOut
+    private int itemCode;
+    private Item item;
+    private boolean soldOut;
+
+    public Item getItem() {
+        return this.item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getItemCode() {
+        return this.itemCode;
+    }
+
+    public void setItemCode(int itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public boolean isSoldOut() {
+        return this.soldOut;
+    }
+
+    public void setSoldOut(boolean soldOut) {
+        this.soldOut = soldOut;
+    }
+
+
+}
