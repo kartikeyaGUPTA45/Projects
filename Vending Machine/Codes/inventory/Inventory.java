3cf25d091bbfd9605ceb51fe92ad06f36f943b37
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 09:29:26 +0530

    Create Inventory.java

---
+++ Vending Machine/Codes/inventory/Inventory.java
@@ -0,0 +1,61 @@
+package inventory;
+
+import item.ItemShelf;
+import item.Item;
+
+public class Inventory {
+    private ItemShelf[] inventory;
+
+    public Inventory(int n) {
+        inventory = new ItemShelf[n];
+        initialEmptyInventory();
+    }
+
+    public ItemShelf[] getInventory() {
+        return this.inventory;
+    }
+
+    public void setInventory(ItemShelf[] inventory) {
+        this.inventory = inventory;
+    }
+
+    public void initialEmptyInventory() {
+        int startCode = 101;
+        for(int i=0;i<inventory.length;i++) {
+            ItemShelf itemShelf = new ItemShelf();
+            itemShelf.setItemCode(startCode);
+            itemShelf.setSoldOut(true);
+            inventory[i] = itemShelf;
+            startCode++;
+
+        }
+    }
+
+    public void addItem(Item item, int itemCode) { 
+        for(ItemShelf shelf: inventory) {
+            if (shelf.getItemCode() == itemCode) {
+                if (shelf.isSoldOut() == true) {
+                    shelf.setItem(item);
+                    shelf.setSoldOut(false);
+                }
+            }
+        }
+    }
+
+    public Item getItem(int itemCode) {
+        for(ItemShelf shelf: inventory) {
+            if (shelf.getItemCode() == itemCode) {
+                if (shelf.isSoldOut()) {
+                    System.out.println("Item is already soldOut");
+                    return new Item();
+                } else {
+                    return shelf.getItem();
+                }
+            }
+        }
+
+        System.out.println("Invalid item Code");
+        return new Item();
+    }
+
+}
