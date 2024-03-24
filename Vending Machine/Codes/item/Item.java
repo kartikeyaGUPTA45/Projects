9050e756d5ee8e8153cea4dcb4c6ff5398b2570e
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 09:29:58 +0530

    Create Item.java

---
+++ Vending Machine/Codes/item/Item.java
@@ -0,0 +1,23 @@
+package item;
+
+public class Item {
+    private ItemType itemType;
+    private int price;
+
+
+    public ItemType getType() {
+        return this.itemType;
+    }
+
+    public void setItemType(ItemType itemType) {
+        this.itemType = itemType;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
