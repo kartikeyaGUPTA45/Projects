fce78558a02db2a81e5c347938a620b07ba3dfa2
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-24 09:28:06 +0530

    Create App.java

---
+++ Vending Machine/Codes/App.java
@@ -0,0 +1,8 @@
+import coin.Coin;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int value = Coin.FIFTY.getValue();
+        System.out.println(value);
+    }
+}
