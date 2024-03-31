6ea0f60373f1199804db07c2958ce25245a66ca1
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-03-31 10:53:17 +0530

    Update ItemType.java

--- Vending Machine/Codes/item/ItemType.java
+++ Vending Machine/Codes/item/ItemType.java
@@ -1,12 +1,21 @@
 package item;
 
 public enum ItemType {
-    OREO,
-    JUICE,
-    COKE,
-    SODA,
-    CHIPS,
-    CHOCOLATE,
-    CANDY,
-    GUM
+    OREO("OREO"), // i = 0,8
+    JUICE("JUICE"), // i =1,9
+    COKE("COKE"), // i =2 
+    SODA("SODA"), // i = 3
+    CHIPS("CHIPS"), // i = 4
+    CHOCOLATE("CHOCOLATE"), // i =5
+    CANDY("CANDY"), // i =6
+    GUM("GUM"); // i=7
+
+    private final String type;
+    ItemType (String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
 }
