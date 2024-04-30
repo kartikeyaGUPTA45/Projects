d9470899e4d625232fb8f609c6989991cc062364
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-04-30 19:21:34 +0530

    Create Condition.java

---
+++ ATM SYSTEM/Condition.java
@@ -0,0 +1,18 @@
+public enum Condition {
+    WORKING("Working"),
+    TECHNICAL_ERROR("Technical Error"),
+    OUT_OF_BALANCE("Out of Balance"),
+    ABANDONED("Abandoned");
+
+
+    private String val;
+
+    Condition(String val) {
+        this.val = val;
+    }
+
+    public String getVal() {
+        return this.val;
+    }
+
+}
